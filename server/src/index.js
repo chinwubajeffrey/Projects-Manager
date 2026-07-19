711ae00477aa596df39d05d56332873a79f51ea4
Chinwuba <chinwubajeffrey55@gmail.com>
2026-07-19 20:57:49 +0100

    quicj one

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://projects-manager-ochre.vercel.app/",
+    origin: "https://projects-manager-ochre.vercel.app",
     credentials: true,
   }),
 );
@@ -21,7 +21,7 @@ app.use(
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "https://projects-manager-ochre.vercel.app/",
+    origin: "https://projects-manager-ochre.vercel.app",
     credentials: true,
   },
 });
